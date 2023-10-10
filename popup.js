0e569ba589b9a83b4cc2a7b623ff6977430c8228
Shravan Vasista <maveric@Shravans-MacBook-Air.local>
2023-10-10 16:40:53 -0700

    Init

---
+++ popup.js
@@ -0,0 +1,8 @@
+const CONSTANTS = {
+    LinkedInProfile: "https://www.linkedin.com/in/shravan-vasista-a14b35115/",
+    GithubProfile: "https://github.com/ssvasista",
+    // Add more constants as needed
+  };
+  
+  document.getElementById("constants-display").innerText = JSON.stringify(CONSTANTS, null, 2);
+  
